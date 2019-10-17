68c9000b26e137de8093b6e998eeda76625e3ac6
Aluno Bandtec <github@alunos.bandtec.com.br>
2019-10-17 12:32:00 -0300

    atualizei

--- meusitezao/funcoes.js
+++ meusitezao/funcoes.js
@@ -2,10 +2,15 @@ function validar(){
     var valor_email = email.value;
     var valor_senha = senha.value;
     var valor_confirmacao = confirmacao.value;
-    var time_coracao = times2.value;
+    var valor_select = times2.value;
     if(valor_email.indexOf('@') != -1 && valor_senha=='futebol' && valor_confirmacao==valor_senha){
         div_mensagem.innerHTML = 'Usuário autenticado';
-        fotofundo.src= `${time_coracao}.jpg`;
+        if(valor_select == "saopaulo"){
+            fotofundo.src = "./tricolor.svg";
+            if(valor_select == "curintias"){
+                fotofundo.src = "./gamba.png";
+            }
+        }
     }
     else{
         div_mensagem1.innerHTML = 'Falha na autentificação';
