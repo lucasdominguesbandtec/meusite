531fc0daad1f0e2b7a5afb68d9e718db922d39b5
Aluno Bandtec <github@alunos.bandtec.com.br>
2019-10-28 15:00:37 -0200

    terminei a tela de cadastro e login

--- meusitezao/funcoes.js
+++ meusitezao/funcoes.js
@@ -1,22 +1,12 @@
 function cadastrar(){
+    div_mensagem1.innerHTML = '';
     var valor_email = email.value;
     var valor_senha = senha.value;
     var valor_confirmacao = confirmacao.value;
-    var valor_select = times2.value;
-    if(valor_email.indexOf('@') != -1 && valor_senha=='futebol' && valor_confirmacao==valor_senha){
+   
+    if(valor_email.indexOf('@') != -1 && valor_senha=='futebol' && valor_confirmacao==valor_senha){ 
+        window.location.href = "login.html";
          alert('Usuário autenticado');
-        if(valor_select == "saopaulo"){
-            fotofundo.src = "./saopaulu.jpg";
-        }
-        if(valor_select == "curintias"){
-            fotofundo.src = "./corinthians.jpeg";
-        }
-        if(valor_select == "santus"){
-            fotofundo.src = "./santus.png";
-        }
-        if(valor_select == "sem_mundial"){
-            fotofundo.src = "palmeirass.jpg";
-        }
     }
     else{
         div_mensagem1.innerHTML = 'Falha na autentificação';
@@ -28,8 +18,21 @@ function validar(){
     mensa.innerHTML = '';
     var valor_email1 = email1.value;
     var valor_senha1 = senha1.value;
+    var valor_select = times2.value;
     if(valor_email1.indexOf('@') != -1 && valor_senha1 =='futebol'){
         alert('Bem vindo');
+        if(valor_select == "saopaulo"){
+            fotolog.src = "saopaulu.jpg";
+        }
+        if(valor_select == "curintias"){
+            fotolog.src = "corinthians.jpeg";
+        }
+        if(valor_select == "santus"){
+            fotolog.src = "santus.png";
+        } 
+        if(valor_select == "sem_mundial"){
+            fotolog.src = "palmeirass.jpg";
+        }
     }
     else{
         mensa.innerHTML = 'Falha na autentificação';
