c98978bc3f95762d11890ea2d46c070832c09d46
Aluno Bandtec <github@alunos.bandtec.com.br>
2019-11-29 14:35:08 -0200

    Atualizado o site completo e ligação ao bd

---
+++ meusitezao/projeto-site/config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  development: {
+    dialect: "sqlite",
+    storage: "./db.development.sqlite"
+  },
+  test: {
+    dialect: "sqlite",
+    storage: ":memory:"
+  },
+  production: {
+    username: 'root',
+    password: 'bandtec',
+    database: 'meubd',
+    host: 'localhost',
+    dialect: 'mysql',
+    xuse_env_variable: 'DATABASE_URL',
+    dialectOptions: {
+      options: {
+        encrypt: true
+      }
+    }
+  }
+};
+ 
