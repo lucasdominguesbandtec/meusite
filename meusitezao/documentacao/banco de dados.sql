f53362c673cabe666a975afffa5d31f360f44d20
lucasdominguesbandtec <lucas.domingues@bandtec.com.br>
2019-11-30 01:22:07 -0200

    nova atualização

--- meusitezao/documentacao/banco de dados.sql
+++ meusitezao/documentacao/banco de dados.sql
@@ -1,16 +1,12 @@
 create database meubd;
 use meubd;
 create  table TB_Cadastro (
-idCadastro int primary key,
+idCadastro int primary key auto_increment,
 Nome varchar(50),
 CPF char(14) unique,
 Email varchar(50),
 Senha char(7)
 );
-create table TB_Login (
-idlogin int primary key,
-Email varchar(50),
-Senha char(7),
-Time_coracao varchar(20)
-);
+
+
  
